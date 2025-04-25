29693083fab42f211f948175cd75a5ebd1b861ed
Sriharsha-go <sriharsham.go@gmail.com>
2025-04-25 15:16:13 +0300

    Update admin.py

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import File, Folder, User, AuditLog
+
+admin.site.register(File)
+admin.site.register(Folder)
+admin.site.register(User)
+admin.site.register(AuditLog)
 
 # Register your models here.
